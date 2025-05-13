4eff9435aa5c5322357007d7ef76ca7b13945d48
Alejandro Polo <polomolinaalejandro@gmail.com>
2025-05-13 13:26:17 +0200

    [feat] : Modified PINN class to impose hard constraint on the initial value

--- Scripts/models/PEMElectrolyzerPINN.py
+++ Scripts/models/PEMElectrolyzerPINN.py
@@ -91,8 +91,12 @@ class PEMElectrolyzerPINN(nn.Module):
         features = self.hidden(x)
         # Outputs will automatically be float64 due to layer definitions
         # Apply exponential activation to ensure positive outputs
-        y1 = torch.exp(self.output1(features))  # First output, enforced to be positive
-        y2 = torch.exp(self.output2(features))  # Second output, enforced to be positive
+        # y1 = torch.exp(self.output1(features))  # First output, enforced to be positive
+        # # y2 = torch.exp(self.output2(features))  # Second output, enforced to be positive
+        # y2 = self.output2(features)  # Second output, enforced to be positive
+
+        y1 = self.y01 + (x - self.t0) * torch.exp(self.output1(features))
+        y2 = self.y02 + (x - self.t0) * self.output2(features)
         return y1, y2
     
     def physics_loss(self, t_phys, x_phys, ode_residual_f_func, ode_residual_g_func, lambda_phys_f, lambda_phys_g):
